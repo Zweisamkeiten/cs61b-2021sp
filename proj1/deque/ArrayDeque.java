43acf3a5222e9607005e19daa5803e08b70a05a6
Einsam <snitxmhm@gmail.com>
2023-03-28 20:47:42 +0800

    proj1: 1. Array List Deque & all tests passed

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,146 @@
+package deque;
+
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private static final int INITIAL_SIZE = 8;
+    private int amount;
+
+    /**
+     * Creates an empty list.
+     */
+    public ArrayDeque() {
+        items = (T[]) new Object[INITIAL_SIZE];
+        size = 0;
+        amount = INITIAL_SIZE;
+        nextFirst = 4;
+        nextLast = 5;
+    }
+
+    private int indexForward(int idx) {
+        return (idx + 1) % amount;
+    }
+
+    private int indexBackward(int idx) {
+        return (idx + amount - 1) % amount;
+    }
+
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        int idx = indexForward(nextFirst);
+
+        while (idx != 0) {
+            a[capacity - (amount - idx)] = items[idx];
+            idx = indexForward(idx);
+        }
+
+        while (idx != nextLast) {
+            a[idx] = items[idx];
+            idx = indexForward(idx);
+        }
+
+        items = a;
+        nextFirst = ((capacity - (amount - idx)) + capacity - 1) % capacity;
+        amount = capacity;
+    }
+
+    public void addFirst(T item) {
+        if (size + 1 > amount) {
+            resize(2 * amount);
+        }
+        size += 1;
+        items[nextFirst] = item;
+        nextFirst = indexBackward(nextFirst);
+    }
+
+    /**
+     * get the first item.
+     *
+     * @return T
+     */
+    public T getFirst() {
+        return items[indexForward(nextFirst)];
+    }
+
+    /**
+     * Adds an item to the end of the list.
+     */
+    public void addLast(T item) {
+        if (size + 1 > amount) {
+            resize(2 * amount);
+        }
+        size += 1;
+        items[nextLast] = item;
+        nextLast = indexForward(nextLast);
+    }
+
+    public T getLast() {
+        return items[indexBackward(nextLast)];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * return list size.
+     *
+     * @return int
+     */
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        int idx = indexForward(nextFirst);
+        while (idx != nextLast) {
+            System.out.print(items[idx] + " ");
+            idx = indexForward(idx);
+        }
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        int toRemoveidx = indexForward(nextFirst);
+        T res = items[toRemoveidx];
+        items[toRemoveidx] = null;
+        size -= 1;
+        nextFirst = indexForward(nextFirst);
+        return res;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        int toRemoveidx = indexBackward(nextLast);
+        T res = items[toRemoveidx];
+        items[toRemoveidx] = null;
+        size -= 1;
+        nextLast = indexBackward(nextLast);
+        return res;
+    }
+
+    private boolean indexWithinArr(int idx) {
+        if (nextFirst < nextLast) {
+            return idx > nextFirst && idx < nextLast;
+        } else {
+            return idx < nextLast || idx > nextFirst;
+        }
+    }
+
+    public T get(int index) {
+        if (index > size - 1) {
+            return null;
+        }
+
+        if (indexWithinArr(index)) {
+            return items[index];
+        }
+        return null;
+    }
+}
