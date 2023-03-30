09afb3d8020df523993d8cd2d40bf22b6f5cd25e
Einsam <snitxmhm@gmail.com>
2023-03-30 16:06:21 +0800

    proj1: MaxArrayDeque

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,45 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private Comparator<T> comparator;
+
+    /**
+     * creates a MaxArrayDeque with the given Comparator.
+     */
+    public MaxArrayDeque(Comparator<T> c) {
+        comparator = c;
+    }
+
+    /**
+     * returns the maximum element in the deque as governed by the previously
+     * given Comparator. If the MaxArrayDeque is empty, simply return null
+     */
+    public T max() {
+        return max(comparator);
+    }
+
+    /**
+     * returns the maximum element in the deque as governed by the parameter
+     * Comparator c. If the MaxArrayDeque is empty, simple return null.
+     */
+    public T max(Comparator<T> c) {
+        if (size() == 0) {
+            return null;
+        }
+        T retMax = getFirst();
+        for (int i = 0; i < size(); i++) {
+            T cur = get(i);
+
+            if (cur == null) {
+                continue;
+            }
+            
+            if (c.compare(retMax, cur) < 0) {
+                retMax = cur;
+            }
+        }
+        return retMax;
+    }
+}
---
+++ proj1/deque/MaxArrayDequeTest.java
@@ -0,0 +1,58 @@
+package deque;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.junit.Assert.assertEquals;
+
+class Dog implements Comparable<Dog> {
+    private static class NaturalComparator implements Comparator<Dog> {
+        public int compare(Dog a, Dog b) {
+            return a.compareTo(b);
+        }
+    }
+
+    private static class NameComparator implements Comparator<Dog> {
+        public int compare(Dog a, Dog b) {
+            return a.name.compareTo(b.name);
+        }
+    }
+
+    public static Comparator<Dog> getNaturalComparator() {
+        return new NaturalComparator();
+    }
+
+    public static Comparator<Dog> getNameComparator() {
+        return new NameComparator();
+    }
+
+    int size;
+    String name;
+
+    public Dog(String n, int s) {
+        name = n;
+        size = s;
+    }
+
+    public void bark() {
+        System.out.println(name + " barked!");
+    }
+
+    @Override
+    public int compareTo(Dog otherDog) {
+        return size - otherDog.size;
+    }
+}
+
+public class MaxArrayDequeTest {
+    @Test
+    public void Test() {
+        MaxArrayDeque<Dog> dogs = new MaxArrayDeque<>(Dog.getNaturalComparator());
+        dogs.addLast(new Dog("Cadell", 8));
+        dogs.addLast(new Dog("Grey", 2));
+        dogs.addLast(new Dog("Nico", 5));
+        Dog maxDog = dogs.max();
+        assertEquals("Cadell", maxDog.name);
+    }
+}
